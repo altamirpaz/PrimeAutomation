bb6dbc73423b03610bfc871fb29504c602c9025c
altamirpaz <a.rodryguez@gmail.com>
2018-11-24 14:21:38 -0200

    Util add

---
+++ PrimeAutomation/src/main/java/Util/RunnerTest.java
@@ -0,0 +1,13 @@
+package Util;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/resources/features",
+        glue = {"stepsw", "util"}
+)
+public class RunnerTest {
+}
